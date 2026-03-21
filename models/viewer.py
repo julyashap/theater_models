8334106a678aedc69c73a582edae9ef33d4aaab3
Julya Shapaeva <shapaeva_julya@mail.ru>
2026-03-22 00:00:40 +0300

    feat(models): Implement Viewer model

---
+++ models/viewer.py
@@ -0,0 +1,18 @@
+from typing_extensions import Annotated
+from pydantic import BaseModel, ConfigDict, Field
+from models.type_aliases import ModelID, NameSurname
+
+
+class Viewer(BaseModel):
+    """Модель зрителя."""
+
+    id: ModelID
+    name: NameSurname
+    surname: NameSurname
+    email_address: Annotated[str, Field(pattern=r"[^@]+@[^@]+\.[^@]+")]
+
+    ticket_number: Annotated[str, Field(min_length=8, max_length=8, pattern=r"^\d+$")]
+
+    phone_number: Annotated[str | None, Field(default=None, pattern=r"^\+\d{1,3}\s?\d{4,14}$")]
+
+    model_config = ConfigDict(str_strip_whitespace=True)
