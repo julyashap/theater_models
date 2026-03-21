b22e1daee8296e9e19b8b761277eeaefb8ae5c10
Julya Shapaeva <shapaeva_julya@mail.ru>
2026-03-22 00:01:15 +0300

    feat(models): Implement Production model

---
+++ models/production.py
@@ -0,0 +1,39 @@
+from datetime import time
+from typing import Any, Literal
+from typing_extensions import Annotated
+from pydantic import BaseModel, ConfigDict, Field, field_validator
+from models.actor import Actor
+from models.type_aliases import ModelID, NameSurname
+
+
+class Production(BaseModel):
+    """Модель постановки."""
+
+    id: ModelID
+    name: NameSurname
+    genre: Literal["drama", "comedy", "musical", "tragedy", "other"]
+    
+    actors: list[Actor]
+
+    description: Annotated[str | None, Field(default=None, min_length=10, max_length=500)]
+    age_rating: Annotated[int, Field(default=0, ge=0, le=18)]
+    duration: Annotated[time, Field(default=time(hour=1))]
+    has_intermission: Annotated[bool, Field(default=True)]
+
+    model_config = ConfigDict(str_strip_whitespace=True)
+
+    @field_validator("genre", mode="before")
+    @classmethod
+    def format_genre_lowercase(cls, v: Any) -> Any:
+        """Приводит переданный жанр в строчный формат."""
+        if isinstance(v, str):
+            v = v.lower()
+        return v
+
+    @field_validator("actors")
+    @classmethod
+    def check_unique_actors(cls, v: Any) -> Any:
+        """Проверяет, есть ли в списке актеров повторения."""
+        if len(v) != len({actor.id for actor in v}):
+            raise ValueError("Actors must be unique!")
+        return v
