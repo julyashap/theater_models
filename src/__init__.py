5538dd7ae9a34c30e09ecd4d9b694e653935c022
Julya Shapaeva <shapaeva_julya@mail.ru>
2026-03-26 01:39:32 +0300

    fix: Fix models imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,7 @@
-from .models import Viewer, Actor, Production, Performance
+from .models.viewer import Viewer
+from .models.actor import Actor
+from .models.production import Production
+from .models.performance import Performance
 from .services.actor import can_play_role, filter_actors_for_role
 from .services.production import get_missing_roles, get_cast_list, get_actor_by_role
 from .services.performance import validate_performance
