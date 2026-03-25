1b9bbe397e17294c485b1d55d5b100a2bdc68f8e
Julya Shapaeva <shapaeva_julya@mail.ru>
2026-03-25 15:01:04 +0300

    fix: Replace default factory in ModelID with IDGenerator

--- models/type_aliases.py
+++ models/type_aliases.py
@@ -3,8 +3,10 @@ from uuid import uuid4
 from pydantic import Field
 from typing_extensions import Annotated
 
+from utils.id_generator import IDGenerator
+
 ModelID = Annotated[
-    str, Field(min_length=4, max_length=4, default_factory=lambda: uuid4().hex[:4])
+    int, Field(default_factory=lambda: IDGenerator().get_id())
 ]
 
 DisplayName = Annotated[str, Field(min_length=1)]
