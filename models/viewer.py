60713daf7662b498a6102790705b9c7283be5fbe
Julya Shapaeva <shapaeva_julya@mail.ru>
2026-03-25 15:03:54 +0300

    fix: Update Viewer model with Person

--- models/viewer.py
+++ models/viewer.py
@@ -1,23 +1,10 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import Field
 from typing_extensions import Annotated
 
-from models.type_aliases import DisplayName, ModelID
-
-PhoneNumber = Annotated[
-    str | None, Field(default=None, pattern=r"^\+\d{1,3}\s?\d{4,14}$")
-]
+from models.person import Person
 
 
-class Viewer(BaseModel):
+class Viewer(Person):
     """Модель зрителя."""
 
-    id: ModelID
-    name: DisplayName
-    surname: DisplayName
-    email_address: Annotated[str, Field(pattern=r"[^@]+@[^@]+\.[^@]+")]
-
-    ticket_number: Annotated[str, Field(min_length=8, max_length=8, pattern=r"^\d+$")]
-
-    phone_number: PhoneNumber
-
-    model_config = ConfigDict(str_strip_whitespace=True)
+    ticket_number: Annotated[str, Field(pattern=r"^\d{8}$")]
