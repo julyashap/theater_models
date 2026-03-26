f2eb148b21ac3338cac6ff40692da303f11352ff
Julya Shapaeva <shapaeva_julya@mail.ru>
2026-03-26 21:55:29 +0300

    fix: Add read function for long_description field

--- setup.py
+++ setup.py
@@ -3,14 +3,22 @@ from setuptools import setup, find_packages
 import os
 
 
-def read_dependencies(file_name):
-    """Считывает зависимости из requirements.txt"""
+def read_dependencies(file_name: str) -> list[str]:
+    """Считывает зависимости из requirements.txt."""
     filepath = os.path.join(os.path.dirname(__file__), file_name)
     with open(filepath, "r", encoding="utf-8") as f:
         dependencies = [
             line.strip() for line in f if line.strip() and not line.startswith("#")
         ]
     return dependencies
+
+
+def read_long_description(file_name) -> str:
+    """Считывает описание из переданного файла."""
+    filepath = os.path.join(os.path.dirname(__file__), file_name)
+    with open(filepath, "r", encoding="utf-8") as f:
+        description = f.read()
+    return description
 
 
 if __name__ == "__main__":
@@ -24,7 +32,7 @@ if __name__ == "__main__":
         author="Julya Shapaeva",
         author_email="shapaeva_julya@mail.ru",
         description="Library with theater models and services.",
-        long_description=open("README.md").read(),
+        long_description=read_long_description("README.md"),
         long_description_content_type="text/markdown",
         url="https://github.com/julyashap/theater_models",
     )
